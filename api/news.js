0d6ce18b443eb418eacf054e52932401684675c0
İsmail Boğaç Ayvaz <ismailbogacayvaz@Bogac-MacBook-Air.local>
2026-03-29 00:48:05 +0300

    final: vercel config and api cors fix

--- api/news.js
+++ api/news.js
@@ -1,13 +1,13 @@
 export default async function handler(req, res) {
   const { rss_url } = req.query;
-  if (!rss_url) return res.status(400).json({ error: "RSS URL gerekli" });
-
   try {
-    // rss2json servisini bir "sunucu" gibi kullanıyoruz
     const response = await fetch(`https://api.rss2json.com/v1/api.json?rss_url=${encodeURIComponent(rss_url)}&api_key=oyncyf0mgh8v7e5lq9w5z9yqyv8u78moxg8p9r9j`);
     const data = await response.json();
+    
+    // Tarayıcıya "bu veri güvenlidir" diyoruz (CORS Fix)
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error: "Veri çekilemedi" });
+    res.status(500).json({ error: "Sunucu haber çekemedi" });
   }
 }
